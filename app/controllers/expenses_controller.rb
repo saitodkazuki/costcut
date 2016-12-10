389106aca25103129ffb2aeabe580ac8b3187ab6
Kazuki <Kazuki@Kazuki-Mac.local>
2016-12-10 17:18:41 +0900

    disabled new.html.haml and implemented the same function in index.html.haml

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,17 +1,26 @@
 class ExpensesController < ApplicationController
   before_action :set_expense, only: [:show, :edit, :update, :destroy]
 
+  # My Method
+  def merge_params(tmp_params)
+    # merge all into paid_at
+    if tmp_params[:paid_at_ampm] == "AM" then
+      tmp_params[:paid_at] = tmp_params[:paid_at_year] + "-" + tmp_params[:paid_at_month] + "-" + tmp_params[:paid_at_day] + " " + tmp_params[:paid_at_oclock] + ":00:00"
+    else
+      tmp_params[:paid_at] = tmp_params[:paid_at_year] + "-" + tmp_params[:paid_at_month] + "-" + tmp_params[:paid_at_day] + " " + ((tmp_params[:paid_at_oclock].to_i + 12) % 24).to_s + ":30:00"
+    end
+    # restructuring
+    tmp_params.delete(:paid_at_year)
+    tmp_params.delete(:paid_at_month)
+    tmp_params.delete(:paid_at_day)
+    tmp_params.delete(:paid_at_ampm)
+    tmp_params.delete(:paid_at_oclock)
+    return tmp_params
+  end
+
   # GET /expenses
   def index
     @expenses = Expense.all
-  end
-
-  # GET /expenses/1
-  def show
-  end
-
-  # GET /expenses/new
-  def new
     @expense = Expense.new
     t = Time.now
     @now_year = t.strftime("%Y").to_i
@@ -20,26 +29,26 @@ class ExpensesController < ApplicationController
     @now_oclock = t.strftime("%H").to_i
   end
 
+  # GET /expenses/1
+  def show
+  end
+
+  # GET /expenses/new
+  # def new
+  # end
+
   # GET /expenses/1/edit
   def edit
+    t = @expense.paid_at
+    @now_year = t.strftime("%Y").to_i
+    @now_month = t.strftime("%m").to_i
+    @now_day = t.strftime("%d").to_i
+    @now_oclock = t.strftime("%H").to_i
   end
 
   # POST /expenses
   def create
-    # merge :paid_at_year, month, day, ampm, and oclock into :paid_at
-    tmp_params = expense_params
-    if tmp_params[:paid_at_ampm] == "AM" then
-      tmp_params[:paid_at] = tmp_params[:paid_at_year] + "-" + tmp_params[:paid_at_month] + "-" + tmp_params[:paid_at_day] + " " + tmp_params[:paid_at_oclock] + ":00:00"
-    else
-      tmp_params[:paid_at] = tmp_params[:paid_at_year] + "-" + tmp_params[:paid_at_month] + "-" + tmp_params[:paid_at_day] + " " + ((tmp_params[:paid_at_oclock].to_i + 12) % 24).to_s + ":00:00"
-    end
-    tmp_params.delete(:paid_at_year)
-    tmp_params.delete(:paid_at_month)
-    tmp_params.delete(:paid_at_day)
-    tmp_params.delete(:paid_at_ampm)
-    tmp_params.delete(:paid_at_oclock)
-
-    @expense = Expense.new(tmp_params)
+    @expense = Expense.new(merge_params(expense_params))
     if @expense.save
       redirect_to @expense, notice: 'Expense was successfully created.'
     else
@@ -49,7 +58,7 @@ class ExpensesController < ApplicationController
 
   # PATCH/PUT /expenses/1
   def update
-    if @expense.update(expense_params)
+    if @expense.update(merge_params(expense_params))
       redirect_to @expense, notice: 'Expense was successfully updated.'
     else
       render :edit
