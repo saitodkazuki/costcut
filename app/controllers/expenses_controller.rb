e531ff3a93a274a36be00c2238f1ab7fe171f42b
Kazuki <Kazuki@Kazuki-Mac.local>
2016-12-19 00:10:55 +0900

    top page done

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -28,6 +28,26 @@ class ExpensesController < ApplicationController
     return tmp_params
   end
 
+  # My Method yesterday(year, month) returns [year, month, day]
+  def yesterday(year, month)
+    if (month -= 1) == 0 then
+      month = 12
+      year -= 1
+    end
+    if [1, 3, 5, 7, 8, 10, 12].include?(month) then
+      day = 31
+    elsif month == 2 then
+      if year % 4 == 0 then
+        day = 29
+      else
+        day = 28
+      end
+    else
+      day = 30
+    end
+    return [year, month, day]
+  end
+
   # GET /expenses
   def index
     @expenses = Expense.all.order('paid_at DESC')
@@ -40,49 +60,54 @@ class ExpensesController < ApplicationController
     @now_day = t.strftime("%d").to_i
     @now_oclock = t.strftime("%H").to_i
 
-    #for graph
+    # for graph
     @graph = Array.new
-    graph_month = @now_month
-    graph_day = @now_day
-    graph_amount = 0
-    count = 7
+    graph_year = @now_year # 初期化（グラフ対象日）
+    graph_month = @now_month # 初期化（グラフ対象日）
+    graph_day = @now_day # 初期化（グラフ対象日）
+    graph_amount = 0 # 初期化（グラフ対象日の出費合計）
+    day_count = 7 # グラフに表示する日数
+    size = @expenses.size
     @expenses.each do |expense|
       if expense.paid_at.strftime("%d").to_i == graph_day then
+        # 今回取得したexpenseデータが、グラフ対象日の新規データであれば、加算して保留
         graph_amount += expense.amount
       else
-        loop do
-          # graph_date = graph_year.to_s + "-" + graph_month.to_s + "-" + graph_day.to_s
-          @graph << [count, graph_amount]
-          # break?
-          count -= 1
-          if count == 0 then
-            break
+        # グラフ対象日のデータを登録（これまで加算されてきたもの、もしくは0）
+        @graph << [graph_day, graph_amount]
+        # グラフに表示する日数分のデータを登録していたら終了
+        if (day_count -= 1) == 0 then
+          return
+        end
+        # 1日前の日付を計算
+        graph_day -= 1
+        if graph_day == 0 then
+          graph_year = yesterday(graph_year, graph_month)[0]
+          graph_month = yesterday(graph_year, graph_month)[1]
+          graph_day = yesterday(graph_year, graph_month)[2]
+        end
+        # 今回取得したexpenseデータの日付になるまで全て0で登録
+        while (graph_day) != expense.paid_at.strftime("%d").to_i do
+          @graph << [graph_day, 0]
+          # グラフに表示する日数分のデータを登録していたら終了
+          if (day_count -= 1) == 0 then
+            return
           end
-          # update graph_day, graph_amount
+          # 1日前の日付を計算
           graph_day -= 1
-          graph_amount = 0
-          if expense.paid_at.strftime("%d").to_i == graph_day then
-            graph_amount = expense.amount
-            next
-          end
           if graph_day == 0 then
-            graph_month -= 1
-            if graph_month == 0 then
-              graph_month == 12
-            end
-            if [1, 3, 5, 7, 8, 10, 12].include?(graph_month) then
-              graph_day = 31
-            elsif graph_month == 2 then
-              if @now_year % 4 == 0 then
-                graph_day = 29
-              else
-                graph_day = 28
-              end
-            else
-              graph_day = 30
-            end
+            graph_year = yesterday(graph_year, graph_month)[0]
+            graph_month = yesterday(graph_year, graph_month)[1]
+            graph_day = yesterday(graph_year, graph_month)[2]
           end
         end
+        # 今回取得したexpenseデータの日付の分のデータを代入
+        graph_amount = expense.amount
+      end
+      # 今回のデータが、最後のデータであるならば終了
+      if (size -= 1) == 0 then
+        # 今回のデータがグラフ対象日もしくはその前日のデータであれば登録して終了
+        @graph << [graph_day, graph_amount]
       end
     end
   end
