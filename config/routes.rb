689791b5373406e8183ab676e023d0346527cb48
Kazuki <Kazuki@Kazuki-Mac.local>
2016-12-23 14:55:17 +0900

    rails g scaffold tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
   resources :expenses
   get 'welcome/index'
   root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206102045) do
+ActiveRecord::Schema.define(version: 20161223054918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20161206102045) do
     t.string   "tag",        null: false
     t.datetime "paid_at",    null: false
     t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
